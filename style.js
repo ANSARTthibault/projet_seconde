d0d3ea480b62cf16dcd80d32faffe312d1073108
ANSARTthibault <150293276+ANSARTthibault@users.noreply.github.com>
2023-11-12 11:13:31 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,15 @@
+//function test() {
+    //alert("bienvenue thib x)");
+    //let ma_div = document.getElementById('ma_div')
+    //ma_div.innerHTML += "yo";
+    //return;
+//}
+function shoot() {
+    var caisse = document.getElementsByClassName('autourcaisse2')
+    alert("yo");
+    caisse.innerHTML = "aaaaaaaaaaaaas";
+    return;
+}
+//addEventListener()
+//.values
+
